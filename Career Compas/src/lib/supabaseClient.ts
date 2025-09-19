67b078f138501d38bd8b2ef3d785574b1bb9165c
Venkateshaa-TD <venkateshaathiyagarajan@gmail.com>
2025-09-19 16:41:41 +0530

    Update supabaseClient.ts

--- Career Compas/src/lib/supabaseClient.ts
+++ Career Compas/src/lib/supabaseClient.ts
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'https://ttjxbqbtrufpnftcraod.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0anhicWJ0cnVmcG5mdGNyYW9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTA4NTcsImV4cCI6MjA3MzgyNjg1N30.s6APPtpQkjUI2nRIUSR0CtnIQOTq9VHbtz2tHw_ua_I';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
